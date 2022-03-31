3c7652d765bbd47c123820d5865808d15d249356
manonm <manon.myung@mkodo.com>
2022-03-31 14:11:19 +0100

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Manon Myung on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
